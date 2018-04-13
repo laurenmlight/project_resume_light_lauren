15703aa638104e16013eb0db100ffe5315cb1657
Lauren Light <laurenmlight@ufl.edu>
2018-04-12 22:32:32 -0400

    add document ready function to js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-$('section').verticalDotNav({
+$(document).ready(function() {
+
+    $('section').verticalDotNav({
 		dot_size: 5,
 		dot_color: "#58585b",
 		nav_color: "#e1e7ec"
 	});
 
-var scroll = new SmoothScroll('a[href*="#"]');
+    var scroll = new SmoothScroll('a[href*="#"]');
+
+});
